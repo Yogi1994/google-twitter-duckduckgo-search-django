1bf9bd5094d5651b9bb7d9696fbb988c037c7bab
Yogesh Chauhan <anujsingh432@gmail.com>
2017-05-07 23:32:49 +0530

    added threads

--- three_search_engine/views.py
+++ three_search_engine/views.py
@@ -8,35 +8,38 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 import time
 import oauth2 as oauth
+import threading
 
-def index(request):
-    response_data = {}
-    response_data['result'] = 'error'
-    response_data['message'] = 'Some error message'
-    query_string = request.GET["q"]
-    # response['google']  = 
-    # return HttpResponse("Hello, world. You're at the polls index.")
+def google_request(query_string, result):
+    google_url ='https://www.googleapis.com/customsearch/v1?key=AIzaSyAk_2v_7k4EAG6j4oe-UExvBCSFXm2U0j4&cx=017576662512468239146:omuauf_lfve&q=' + query_string
 
-    url ='https://www.googleapis.com/customsearch/v1?key=AIzaSyAk_2v_7k4EAG6j4oe-UExvBCSFXm2U0j4&cx=017576662512468239146:omuauf_lfve&q=' + query_string
-    response = requests.get(url)
-    response = response.json()
-    res_google = {}
-    if(len(response['items'])):
-        res_google['text'] = response['items'][0]['title']
-        res_google['url'] = response['items'][0]['link']
+    google_response = requests.get(google_url)
     
-    google = res_google
+    google_response = google_response.json()
+    print google_response
+    res_google = {}
+    if(len(google_response['items'])):
+        res_google['text'] = google_response['items'][0]['title']
+        res_google['url'] = google_response['items'][0]['link']
+    
+    result['google'] = res_google
 
-    url2 = 'http://api.duckduckgo.com/?q=' + query_string + '&format=json'
-    response = requests.get(url2)
-    response = response.json()
+
+def duckduckgo_request(query_string, result):
+    duckduckgo_url = 'http://api.duckduckgo.com/?q=' + query_string + '&format=json'
+
+    duckduckgo_response = requests.get(duckduckgo_url)
+    duckduckgo_response = duckduckgo_response.json()
     res_ddg = {}
-    if(len(response['RelatedTopics'])):
-        res_ddg['text'] = response['RelatedTopics'][0]['Text']
-        res_ddg['url'] = response['RelatedTopics'][0]['FirstURL']
-    duckduckgo = {}
-    duckduckgo['duckduckgo'] =  res_ddg
+    if(len(duckduckgo_response['RelatedTopics'])):
+        res_ddg['text'] = duckduckgo_response['RelatedTopics'][0]['Text']
+        res_ddg['url'] = duckduckgo_response['RelatedTopics'][0]['FirstURL']
+    # duckduckgo = {}
+    # duckduckgo['duckduckgo'] =  res_ddg
+    result['duckduckgo'] = res_ddg
 
+def twitter_request(query_string, result):
+    twitter_url ='https://api.twitter.com/1.1/search/tweets.json?count=1&q=' + query_string
 
     CONSUMER_KEY = "MJmhSwcLw5OEvf3Yj5VbIEUBS"
     CONSUMER_SECRET = "6udAiQT0I6QK2sjkcRABy8Z1cxAtE6RaneU0jOpC3lkYNP5y03"
@@ -46,8 +49,8 @@ def index(request):
     consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
     access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
     client = oauth.Client(consumer, access_token)
-    url ='https://api.twitter.com/1.1/search/tweets.json?count=10&q=' + query_string
-    response, data = client.request(url)
+    
+    twitter_response, data = client.request(twitter_url)
 
     tweets = json.loads(data)
     
@@ -55,13 +58,34 @@ def index(request):
     if(len(tweets['statuses'])):
         res_twitter['text'] = tweets['statuses'][0]['text']
         res_twitter['url'] = tweets['statuses'][0]['entities']['urls']
-    twitter={}
-    twitter['twitter'] = res_twitter
+
+    result['twitter'] = res_twitter
 
 
+def index(request):
 
+    query_string = request.GET["q"]
+    jobs = []
+    result = {}
+
+    # thread = threading.Thread(target=google_request(query_string, result))
+    # jobs.append(thread)
+    thread = threading.Thread(target=duckduckgo_request(query_string, result))
+    jobs.append(thread)
+    thread = threading.Thread(target=twitter_request(query_string, result))
+    jobs.append(thread)
     
-    return JsonResponse({'google':google, 'duckduckgo':duckduckgo, 'twitter':res_twitter})
+
+
+    # Start the threads (i.e. calculate the random number lists)
+    for j in jobs:
+        j.start()
+
+    # Ensure all of the threads have finished
+    for j in jobs:
+        j.join()
+
+    return JsonResponse(result)
 #     GET /1.1/search/tweets.json?q=modi&oauth_consumer_key=MJmhSwcLw5OEvf3Yj5VbIEUBS&oauth_token=211807118-LIhgcDErkXzgBxLDFuIYMVIUv3YqVrCxDb2UImS2&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1494170872&oauth_nonce=1568847899&oauth_version=1.0&oauth_signature=3XjLtUpuL4J6+mk3km6fnIw/BOk= HTTP/1.1
 # Host: api.twitter.com
 # Authorization: OAuth oauth_consumer_key="MJmhSwcLw5OEvf3Yj5VbIEUBS",oauth_token="211807118-LIhgcDErkXzgBxLDFuIYMVIUv3YqVrCxDb2UImS2",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1494173377",oauth_nonce="WEiBpZKtREW",oauth_version="1.0",oauth_signature="Xd%2FyBGHpNtSnvVMAV5qBNEOqK18%3D"
